eb9adacb7ebdb77dfdc9ac05aaa8579d11d522c7
chandra sekhar avula <cavula@innominds.com>
2016-12-30 12:14:31 +0530

    edmp sample app

---
+++ src/main/java/de/codecentric/EdmpSampleAppApplication.java
@@ -0,0 +1,14 @@
+package de.codecentric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdmpSampleAppApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EdmpSampleAppApplication.class, args);
+	}
+	
+	
+}
